1a40c0d1ad25936210cde6d1128f16e4420b3714
Angela Julie Dobbs <ajdobbs.sm@gmail.com>
2016-11-16 10:13:11 -0500

    Fixed all basic tests running correctly

--- app/view1/view1_test.js
+++ app/view1/view1_test.js
@@ -6,12 +6,11 @@ describe('myApp.view1 module', function() {
   beforeEach(module('myApp.jsonService'));
   beforeEach(module('myApp.parseService'));
 
-
   describe('view1 controller', function(){
 
-    it('should ....', inject(function($controller) {
+    it('should ....', inject(function($controller, $rootScope) {
       //spec body
-      var view1Ctrl = $controller('View1Ctrl');
+      var view1Ctrl = $controller('View1Ctrl', {'$scope': $rootScope.$new()});
       expect(view1Ctrl).toBeDefined();
     }));
 
